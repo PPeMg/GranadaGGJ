103b6d1092001e15da20a3558f6db6b415453875
mmibanez <maribhez@gmail.com>
2019-01-26 22:48:05 +0100

    Commit Scena Union
    Commit Scena Union.

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Item : MonoBehaviour
+{
+    private bool _selected;
+
+    private void OnMouseDown()
+    {
+        Click();
+    }
+
+    void Click()
+    {
+        if (_selected)
+            return;
+
+        _selected = true;
+    }
+}
