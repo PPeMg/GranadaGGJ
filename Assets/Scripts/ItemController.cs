c02c1a29d3caaf4f632dda93b50e04e796046894
mmibanez <maribhez@gmail.com>
2019-01-27 00:00:12 +0100

    Commit
    Commit

--- Assets/Scripts/ItemController.cs
+++ Assets/Scripts/ItemController.cs
@@ -1,21 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemController : MonoBehaviour
 {
     private int id;
     private string itemName;
+    private bool selected;
 
     void Awake()
     {
         id = -1;
         itemName = "";
+        selected = false; 
+    }
+
+    private void Start()
+    {
+        GetComponentInChildren<Button>().onClick.AddListener(Click);
     }
 
     public void InitItemButton(int id, string name)
     {
         this.id = id;
         this.itemName = name;
+    }
+
+    public void SetSelected(bool selected)
+    {
+        this.selected = selected;
+    }
+
+    public void Click()
+    {
+        selected = GameManager.instance.AddSelectedItem(id); 
+        //GameManager.instance.profile.avatarId = this.id;
+        Debug.Log("Pulsado Preferencia con ID: " + this.id);
     }
 }
