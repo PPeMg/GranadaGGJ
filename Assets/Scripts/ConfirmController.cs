32517f5c3ce2286bdd57630c2a1090c6f93d5e81
mmibanez <maribhez@gmail.com>
2019-01-27 00:18:25 +0100

    Confirm Button Commit.

---
+++ Assets/Scripts/ConfirmController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ConfirmController : MonoBehaviour
+{
+    private Button confirmProfileButton;
+
+    void Awake()
+    {
+        confirmProfileButton = GetComponent<Button>();
+        confirmProfileButton.onClick.AddListener(StartCreateProfile);
+    }
+
+    public void StartCreateProfile()
+    {
+        GameManager.instance.profileDB.AddItem(GameManager.instance.profile);
+        GameManager.instance.SetActiveScreen(0);
+    }
+}
