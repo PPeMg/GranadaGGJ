20a6ca4bdaaf77d696ce09c99b03c6c63ff7296f
PPeMg <jdeldo@gmail.com>
2019-01-26 17:56:50 +0100

    Added Create Profile Functionality. Fixed Little errors

---
+++ Assets/Scripts/Functionalitys.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Functionalitys : MonoBehaviour
+{
+    public void PrintMessage(string message)
+    {
+        Debug.Log(message);
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,12 +7,18 @@ public class GameManager : MonoBehaviour
 {
     public GameObject[] menus;
 
+    public static GameManager instance;
+
     void Awake()
     {
-        for (int i = 1; i < menus.Length; i++)
-        {
-            menus[i].SetActive(false);
+        // This make it a Singleton
+        if (instance == null) {
+            instance = this;
+        } else if (instance != this) {
+            Destroy(gameObject);
         }
+
+        SetActiveScreen(0);
     }
     
     void Start()
@@ -31,5 +37,19 @@ public class GameManager : MonoBehaviour
         menus[0].SetActive(false);
         menus[1].SetActive(true);
         yield return null;
+    }
+
+    public void SetActiveScreen(int index)
+    {
+        for(int i = 0; i < menus.Length; i++)
+        {
+            if(i == index)
+            {
+                menus[i].SetActive(true);
+            } else
+            {
+                menus[i].SetActive(false);
+            }
+        }
     }
 }
