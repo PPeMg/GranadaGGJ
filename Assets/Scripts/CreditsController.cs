cba8f06fcc59fa69effea12a20af99c5a8c72ecc
mmibanez <maribhez@gmail.com>
2019-01-27 14:00:07 +0100

    Commit CreditsController

---
+++ Assets/Scripts/CreditsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsController : MonoBehaviour
+{
+    private Button creditButton;
+    private Rect windowRect = new Rect(20, 20, 120, 50);
+    public string text = string.Empty;
+    TextMesh screen; 
+    void Awake()
+    {
+        creditButton = GetComponent<Button>();
+        creditButton.onClick.AddListener(ShowCredits);
+    }
+
+    public void ShowCredits()
+    {
+        //windowRect = GUI.Window(0, windowRect, WindowFunction, "Credits");
+        //text = "prueeeba";
+        //screen = text; 
+    }
+
+}
